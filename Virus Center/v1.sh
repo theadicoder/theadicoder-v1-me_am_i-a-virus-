0d089f63cfdb936ad3a96d6785f479934aa859b2
ADITYA THAKUR <Aadicorian@outlook.com>
2024-03-29 21:21:31 +0530

    Add files via upload

---
+++ Virus Center/v1.sh
@@ -0,0 +1,30 @@
+    # Diceclamer : 
+        
+        # Powerfull Shell Which Perform Like a Mallwire Virus
+          
+sudo su 
+# I'm in adminstrator mode no one type of disputes are here : 
+   
+   apt install python3 
+
+   mkdir Metoport_0.1-v
+cd Metoport_0.1-v
+
+   # I'm installing virus brute : 
+     wget "https://drive.google.com/file/d/1Sksrgu36b-cehcipxL4WxMTKo4G9CACN/view?usp=drive_link"
+
+     screenfetch 
+     whoami 
+
+     # I'm on bording proccessing .. ===============> Please wait for result : 
+
+     # Virus self putting up on System folder : 
+     paste v1.sh > C:\Windows\System32
+
+     # I'm Opening the virus file 
+
+      chmod +x v1.sh v2.bat
+
+
+     # End Every proccess ...
+
---
+++ Virus Center/v2.bat
@@ -0,0 +1,9 @@
+start "" "https://ww.instagram.com"
+start "" "https://chat.openai.com"
+start ""  "https://developer.android.com"
+# Here's an important thing is this is the link to connect with that virus maker person chat-section
+start "" "https://https://twitter.com/theadicoder"
+start "" "https://google.com" 
+start "" "https://udemy.com"
+start "" "https://discord.com"
+
